1f1fe07d86546ffb35f20d9dc166f5fde30889a6
wesley-paiva-vega <wesley.snybool.dev@gmail.com>
2022-06-06 18:42:42 -0300

    first

---
+++ global.styles.ts
@@ -0,0 +1,18 @@
+import { createStitches } from '@stitches/react';
+
+export const { styled, getCssText } = createStitches({
+  theme: {
+    fonts: {
+      system: 'Arial Black',
+    },
+    colors: {
+      hiContrast: 'hsl(206,10%,5%)',
+      loContrast: 'white',
+    },
+    fontSizes: {
+      1: '13px',
+      2: '15px',
+      3: '17px',
+    },
+  },
+});
