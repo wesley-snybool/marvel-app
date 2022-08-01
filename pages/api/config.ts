2acf853645362ffa2d1fb3e12704cda753084252
wesley-paiva-vega <wesley.snybool.dev@gmail.com>
2022-08-01 19:57:23 -0300

    ajuste problema no .env

--- pages/api/config.ts
+++ pages/api/config.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const apikey = process.env.NEXT_API_KEY;
-const hash = process.env.NEXT_APP_HASH;
-const ts = process.env.NEXT_APP_TS;
-const apiUrl = process.env.NEXT_APP_ENDPOINT;
+const apikey = '88afc73c960dd17242a16a42cc8c492b';
+const hash = '08f49c78ef7ee6d774933ae9e5298f71';
+const ts = '1';
+const apiUrl = 'https://gateway.marvel.com/v1/public/';
 
 export const api = axios.create({
   params: {
@@ -11,8 +11,5 @@ export const api = axios.create({
     apikey,
     hash,
   },
-  headers: { Accept: 'text/html, application/json, text/plain, */*' },
   baseURL: apiUrl,
-  proxy: undefined,
-  method: 'get',
 });
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,38 @@
 import type { NextPage } from 'next';
-import { Router, useRouter } from 'next/router';
+import { useRouter } from 'next/router';
+import { styled } from '@stitches/react';
+
+const MainContainer = styled('div', {
+  backgroundColor: '#5F7ED6',
+  width: '100%',
+  height: '100vh',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  button: {
+    width: '250px',
+    height: '75px',
+    borderRadius: '5px',
+    border: 'none',
+    cursor: 'pointer',
+    color: 'DarkBlue',
+
+    '&:hover': {
+      filter: 'brightness(0.95)',
+    },
+  },
+});
 
 const Main: NextPage = () => {
   const router = useRouter();
 
   return (
-    <div>
+    <MainContainer>
       <button onClick={() => router.push('/home')}>
         Faça loguin para continuar
       </button>
-    </div>
+    </MainContainer>
   );
 };
 
